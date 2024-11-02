cff2a94902b079166173acbad365b43fa800162a
Victor Olubunmi <olubunmivictor6@gmail.com>
2024-11-02 10:35:50 +0100

    Bug Fixes

--- app/account/home/page.tsx
+++ app/account/home/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useFormik } from "formik";
 import { Icon } from "@iconify/react";
 import { toast } from "react-toastify";
@@ -10,9 +11,8 @@ import { callSchema } from "@/helpers/validators";
 import { useAuthStateStore } from "@/hooks/authStateStore";
 import { checkBoolean, todayDate } from "@/helpers/common";
 import { Button, Input, Tooltip, User } from "@nextui-org/react";
-
 import { LoadingContainer } from "@/components/loading-container";
-import Link from "next/link";
+
 
 declare global {
     interface Window {
@@ -28,6 +28,7 @@ export default function Home() {
     // wss://webrtc.africastalking.com/connect
 
     // let client: any = null;
+    const [pageLoading, setPageLoading] = useState(true);
     const [client, setClient] = useState<any>(null);
     const [credentials, setCredentials] = useState<any>(null);
     const [token, setToken] = useState<any>(null);
@@ -56,6 +57,8 @@ export default function Home() {
         } else {
             setHasCredentials(false);
         }
+
+        setPageLoading(false);
     }
 
     // Fetch and set user data
@@ -159,8 +162,8 @@ export default function Home() {
         if (!token) {
             const tokenResponse: ApiResponseInterface = await makeRequest(
                 "/api/account/call/token",
-                "POST",
-                {provider_id: credentials?.id},
+                "GET",
+                {},
                 true
             );
     
@@ -247,118 +250,122 @@ export default function Home() {
         handleCallEvents();
     }, [client]);
 
-    return (
-        <>
-            <div className="col-span-full">
-                <div className="relative card bg-heading dark:bg-primary-500 py-7 xl:mb-8">
-                    <div className="flex flex-col-reverse sm:flex-row gap-5 justify-between">
-                        <div className="shrink-0">
-                            <h1 className="text-white text-[25px] leading-none font-semibold flex-center gap-2 justify-start">
-                                <span className="shrink-0">Good Day, {user?.last_name}</span>
-                                <span className="select-none hidden md:inline-block animate-hand-wave origin-[70%_70%]">👋</span>
-                            </h1>
-                            <p className="font-spline_sans text-sm text-dark-text dark:text-white mt-2">We're glad to have you back</p>
-                        </div>
-                        <div className="shrink-0">
-                            <p className="font-spline_sans text-sm !leading-none text-white today">Today is {today}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            {!hasCredentials ? (
+    if (pageLoading) {
+        return (<LoadingContainer/>);
+    } else {
+        return (
+            <>
                 <div className="col-span-full">
-                    <div className="col-span-8 card text-center min-h-52">
-                        <p>Sorry! You do not have any VoIP credentials set. Kindly <Link className="font-bold text-primary" href="/account/provider-settings">click here</Link> to set your own credentials.</p>
+                    <div className="relative card bg-heading dark:bg-primary-500 py-7 xl:mb-8">
+                        <div className="flex flex-col-reverse sm:flex-row gap-5 justify-between">
+                            <div className="shrink-0">
+                                <h1 className="text-white text-[25px] leading-none font-semibold flex-center gap-2 justify-start">
+                                    <span className="shrink-0">Good Day, {user?.last_name}</span>
+                                    <span className="select-none hidden md:inline-block animate-hand-wave origin-[70%_70%]">👋</span>
+                                </h1>
+                                <p className="font-spline_sans text-sm text-dark-text dark:text-white mt-2">We're glad to have you back</p>
+                            </div>
+                            <div className="shrink-0">
+                                <p className="font-spline_sans text-sm !leading-none text-white today">Today is {today}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            ) : (
-                <>
-                {calling && (
+
+                {!hasCredentials ? (
                     <div className="col-span-full">
-                        <div className="*:p-7 grid grid-cols-12 gap-5 mb-3">
-                            <div className="col-span-8 card min-h-32 text-white bg-success *:rounded-15 dark:bg-primary-500">
-                                <div className="flex flex-col justify-center items-center">
-                                    <span>{callStatus}</span>
-                                </div>
-                            </div>
-                            <div className="col-span-4 card *:rounded-15">
-                                <div className="flex flex-row flex-wrap gap-5">
-                                    <Tooltip content="End Call">
-                                        <Button onClick={onHandUp} isIconOnly type="button" color="danger" aria-label="End Call">
-                                            <Icon icon="solar:end-call-linear" fontSize={30} />
-                                        </Button>
-                                    </Tooltip>
-                                    <Tooltip content={mute ? "Unmute": "Mute"}>
-                                        <Button onClick={onHandleMute} disabled={ringing} isIconOnly type="button" color={mute? "primary": "default"} aria-label="Mute">
-                                            {mute? <Icon icon="octicon:unmute-24" fontSize={30} /> : <Icon icon="octicon:mute-24" fontSize={30} />}
-                                        </Button>
-                                    </Tooltip>
-                                </div>
-                            </div>
+                        <div className="col-span-8 card text-center min-h-52">
+                            <p>Sorry! You do not have any VoIP credentials set. Kindly <Link className="font-bold text-primary" href="/account/provider-settings">click here</Link> to set your own credentials.</p>
                         </div>
                     </div>
-                )}
-
-                <div className="col-span-full card">
-                    <div className="*:p-7 *:rounded-15 grid grid-cols-12 gap-4 mb-4">
-                        <div className="col-span-12">
-                            <div className="">
-                                <form className="leading-none mt-8" onSubmit={handleSubmit}>
-                                    <div className="mb-2.5 pt-1">
-                                        <Input
-                                            isRequired
-                                            type="number"
-                                            name="to"
-                                            variant="bordered"
-                                            label="Phone Number"
-                                            labelPlacement="outside"
-                                            onChange={handleChange}
-                                            placeholder="234xxxxxxxxx"
-                                            isInvalid={checkBoolean(errors.to, touched.to)}
-                                            value={values.to}
-                                            errorMessage={errors.to}
-                                            classNames={{
-                                                label: "form-label",
-                                                inputWrapper: "form-input px-4 py-3.5 rounded-lg"
-                                            }}
-                                        />
+                ) : (
+                    <>
+                    {calling && (
+                        <div className="col-span-full">
+                            <div className="*:p-7 grid grid-cols-12 gap-5 mb-3">
+                                <div className="col-span-8 card min-h-32 text-white bg-success *:rounded-15 dark:bg-primary-500">
+                                    <div className="flex flex-col justify-center items-center">
+                                        <span>{callStatus}</span>
                                     </div>
-
-                                    <Button disabled={ringing} isLoading={loading ? true : false} className="btn b-solid btn-primary-solid font-bold w-full" type="submit">{<Icon icon="mdi:call" fontSize={20} />} Call Now</Button>
-                                </form>
+                                </div>
+                                <div className="col-span-4 card *:rounded-15">
+                                    <div className="flex flex-row flex-wrap gap-5">
+                                        <Tooltip content="End Call">
+                                            <Button onClick={onHandUp} isIconOnly type="button" color="danger" aria-label="End Call">
+                                                <Icon icon="solar:end-call-linear" fontSize={30} />
+                                            </Button>
+                                        </Tooltip>
+                                        <Tooltip content={mute ? "Unmute": "Mute"}>
+                                            <Button onClick={onHandleMute} disabled={ringing} isIconOnly type="button" color={mute? "primary": "default"} aria-label="Mute">
+                                                {mute? <Icon icon="octicon:unmute-24" fontSize={30} /> : <Icon icon="octicon:mute-24" fontSize={30} />}
+                                            </Button>
+                                        </Tooltip>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-span-12">
-                            {!loaded && !credentials ? (<LoadingContainer />) : (
-                                <div className="relative w-full">
-                                    <div className="px-2 py-2 flex flex-row justify-between items-center">
-                                        <div className="">Provider</div>
-                                        <div className="">
-                                            <User
-                                                avatarProps={{ radius: "lg", src: `${process.env.NEXT_PUBLIC_APP_BACKEND_URL}${credentials?.provider_image}` }}
-                                                description={credentials?.provider_name}
-                                                name={credentials?.provider_name}
-                                            >
-                                                {credentials?.provider_slug}
-                                            </User>
+                    )}
+
+                    <div className="col-span-full card">
+                        <div className="*:p-7 *:rounded-15 grid grid-cols-12 gap-4 mb-4">
+                            <div className="col-span-12">
+                                <div className="">
+                                    <form className="leading-none mt-8" onSubmit={handleSubmit}>
+                                        <div className="mb-2.5 pt-1">
+                                            <Input
+                                                isRequired
+                                                type="number"
+                                                name="to"
+                                                variant="bordered"
+                                                label="Phone Number"
+                                                labelPlacement="outside"
+                                                onChange={handleChange}
+                                                placeholder="234xxxxxxxxx"
+                                                isInvalid={checkBoolean(errors.to, touched.to)}
+                                                value={values.to}
+                                                errorMessage={errors.to}
+                                                classNames={{
+                                                    label: "form-label",
+                                                    inputWrapper: "form-input px-4 py-3.5 rounded-lg"
+                                                }}
+                                            />
+                                        </div>
+
+                                        <Button disabled={ringing} isLoading={loading ? true : false} className="btn b-solid btn-primary-solid font-bold w-full" type="submit">{<Icon icon="mdi:call" fontSize={20} />} Call Now</Button>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className="col-span-12">
+                                {!loaded && !credentials ? (<LoadingContainer />) : (
+                                    <div className="relative w-full">
+                                        <div className="px-2 py-2 flex flex-row justify-between items-center">
+                                            <div className="">Provider</div>
+                                            <div className="">
+                                                <User
+                                                    avatarProps={{ radius: "lg", src: `${process.env.NEXT_PUBLIC_APP_BACKEND_URL}${credentials?.provider_image}` }}
+                                                    description={credentials?.provider_name}
+                                                    name={credentials?.provider_name}
+                                                >
+                                                    {credentials?.provider_slug}
+                                                </User>
+                                            </div>
+                                        </div>
+                                        <div className="px-2 py-2 flex flex-row justify-between items-center">
+                                            <div className="">Phone Number</div>
+                                            <div className="">{credentials?.credentials.phone}</div>
+                                        </div>
+                                        <div className="px-2 py-2 flex flex-row justify-between items-center">
+                                            <div className="">Username</div>
+                                            <div className="">{credentials?.credentials.username}</div>
                                         </div>
                                     </div>
-                                    <div className="px-2 py-2 flex flex-row justify-between items-center">
-                                        <div className="">Phone Number</div>
-                                        <div className="">{credentials?.credentials.phone}</div>
-                                    </div>
-                                    <div className="px-2 py-2 flex flex-row justify-between items-center">
-                                        <div className="">Username</div>
-                                        <div className="">{credentials?.credentials.username}</div>
-                                    </div>
-                                </div>
-                            )}
+                                )}
+                            </div>
                         </div>
                     </div>
-                </div>
+                </>
+                )}
             </>
-            )}
-        </>
-    );
+        );
+    }
 }
